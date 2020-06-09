713ebb5fdc7191b5ea28d9e5de4e42f899e0b7db
Pravdin Anton <pravdinip@gmail.com>
2020-06-09 23:00:22 +0300

    first commit

---
+++ client/src/components/streams/StreamEdit.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SteamEdit = () => {
+	return (
+		<div>SteamEdit</div>
+	)
+}
+
+export default SteamEdit;
---
+++ client/src/components/streams/StreamList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SteamList = () => {
+	return (
+		<div>SteamList</div>
+	)
+}
+
+export default SteamList;
