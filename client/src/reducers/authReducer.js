934c1c5772cee6c72e8a579a623a1c976db62712
Pravdin Anton <pravdinip@gmail.com>
2020-06-10 15:41:17 +0300

    rewrote app

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,17 +1,17 @@
-import { SIGN_IN, SIGN_OUT } from "../actions/types";
+import { SIGN_IN, SIGN_OUT } from '../actions/types';
 
-const INITIAL_STATE = {
-	isSignedIn: null,
-	userId: null,
+const INTIAL_STATE = {
+  isSignedIn: null,
+  userId: null
 };
 
-export default (state = INITIAL_STATE, action) => {
-	switch (action.type) {
-		case SIGN_IN:
-			return { ...state, isSignedIn: true, userId: action.payload };
-		case SIGN_OUT:
-			return { ...state, isSignedIn: false, userId: null};
-		default:
-			return state;
-	}
+export default (state = INTIAL_STATE, action) => {
+  switch (action.type) {
+    case SIGN_IN:
+      return { ...state, isSignedIn: true, userId: action.payload };
+    case SIGN_OUT:
+      return { ...state, isSignedIn: false, userId: null };
+    default:
+      return state;
+  }
 };
